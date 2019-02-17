bfdb201dbc52fe4cf941d2e725808632aeb639d0
Ed Lui <rewq142857@hotmail.com>
2019-02-17 22:08:49 +0800

    Update get_n_ways_to_traverse_a_mat.py

--- get_n_ways_to_traverse_a_mat.py
+++ get_n_ways_to_traverse_a_mat.py
@@ -1,5 +1,5 @@
-def get_n_ways_to_traverse_a_mat(n,m):
-    if n==1 or m==1:
+def get_n_ways_to_traverse_a_mat(length,width):
+    if length==1 or width==1:
         return 1
-    return get_n_ways_to_traverse_a_mat(n-1,m)+get_n_ways_to_traverse_a_mat(n,m-1)
+    return get_n_ways_to_traverse_a_mat(length-1,m)+get_n_ways_to_traverse_a_mat(length,width-1)
 get_n_ways_to_traverse_a_mat(5,5)
